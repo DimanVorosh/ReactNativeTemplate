cccae9a38a3bda9e7c56704397bf02a9cbbb105b
Дмитрий Ворошилло <dmitry.voroshillo@moonsolution.ru>
2021-09-30 17:25:59 +0300

    Удален console.log

--- template/src/theme/Font.js
+++ template/src/theme/Font.js
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native'
 import { Variables } from './Variables'
 
 export const Font = (family, size, color) => {
-  console.log(family, Variables().fonts.size[size], color)
   return {
     fontFamily: Variables().fonts.family[family],
     fontSize: Variables().fonts.size[size],
